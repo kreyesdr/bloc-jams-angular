876400ce098cc022bb746a48296b3506b8b98ef7
DESKTOP-VSVB76H\Admin <kreyes616@gmail.com>
2017-07-19 01:02:19 -0400

    Done Assignment

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,24 +1,29 @@
 (function() {
   function config($stateProvider, $locationProvider) {
     $locationProvider
-    .html5mode({
-      enable: true,
+    .html5Mode({
+      enabled: true,
       requireBase: false
     });
-  }
 
-  $stateProvider
+
+    $stateProvider
     .state('landing', {
       url: '/',
-      templateUrl: 'templates/landing.html'
+      templateUrl: '/templates/landing.html'
     })
     .state('album', {
       url: '/album',
       templateUrl: '/templates/album.html'
+    })
+    .state('collection', {
+      url: '/collection',
+      templateUrl: '/templates/collection.html'
     });
+  }
 
   angular
-   .module('blocJams', ['ui.router'])
-   .config(config);
+  .module('blocJams', ['ui.router'])
+  .config(config);
 
 })();
