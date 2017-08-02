54e767b83756faad736734d3701f5f89f97ab27b
NOVOSIT\kreyes <kreyes616@gmail.com>
2017-08-02 15:55:02 -0400

    Done with assignment

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,12 +1,9 @@
-(function () {
-  function AlbumCtrl(Fixtures) {
-    this.albumData = Fixtures.getAlbum();
-    for (var i = 0; i < 5; i++) {
-      this.albumData.push(angular.copy(albumPicasso));
+(function() {
+    function AlbumCtrl(Fixtures) {
+        this.albumData = Fixtures.getAlbum();
     }
-  }
 
-  angular
-    .module('blocJams')
-    .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+    angular
+        .module('blocJams')
+        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
 })();
